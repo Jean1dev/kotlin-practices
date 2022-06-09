87802d5872aa914152e5a9bed0cdda7c4efc4b0a
Jeanluca <jeanlucafp@gmail.com>
2022-06-08 21:15:13 -0300

    setup project

---
+++ comercializacao/src/main/kotlin/com/app/comercializacao/ComercializacaoApplication.kt
@@ -0,0 +1,11 @@
+package com.app.comercializacao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComercializacaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ComercializacaoApplication>(*args)
+}
---
+++ comercializacao/src/test/kotlin/com/app/comercializacao/ComercializacaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.comercializacao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComercializacaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
